5aab43fcb3815c435f38dc7105f90b9dbdc52498
ge-felix <1george.1995@googlemail.com>
2023-08-06 20:15:12 +0100

    cloudfront redirect to https

--- cloud-resume/cloudfront.tf
+++ cloud-resume/cloudfront.tf
@@ -34,7 +34,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "https-only"
+    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
